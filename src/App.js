2382cf740afbfbd525b38afbb17717dd688f6036
sekercih <hkkskrc@gmail.com>
2021-06-10 21:31:40 +0300

    flag

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import UseEffect01 from "./UseEffect01";
+import UseEffectAxios from "./UseEffectAxios";
+import UseEffectFetch from "./UseEffectFetch";
+import UseEffectFetchError from "./UseEffectFetchError";
+import UseEffectFetchTxt from "./UseEffectFetchTxt";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseEffect01 /* />
+     
+      {/* <UseEffectFetchError /> */}
+      {/* <UseEffectAxios/> */}
+      {/* <UseEffectFetchTxt/> */}
+      <UseEffectFetch /> }
+    </div>
+  );
+}
+export default App;
